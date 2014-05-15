41a4c1c64412500b41f82a96310de08c2c7ad902
xivk <ben@osmsharp.com>
2014-05-15 15:07:35 +0200

    Added Android.Tiles sample.

---
+++ Android.Tiles/Android.Tiles/Activity1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using OsmSharp.Android.UI;
+using OsmSharp.UI.Map;
+using OsmSharp.UI.Map.Layers;
+using OsmSharp.Math.Geo;
+
+namespace Android.Tiles
+{
+    [Activity(Label = "Android.Tiles", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        /// <summary>
+        /// Holds the mapview.
+        /// </summary>
+        private MapView _mapView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // hide title bar.
+            this.RequestWindowFeature(global::Android.Views.WindowFeatures.NoTitle);
+
+            // initialize OsmSharp native handlers.
+            Native.Initialize();
+
+            // initialize map.
+            var map = new Map();
+
+            // add tile Layer.
+            // WARNING: Always look at usage policies!
+            // WARNING: Don't use my tiles, it's a free account and will shutdown when overused!
+            map.AddLayer(new LayerTile("http://a.tiles.mapbox.com/v3/osmsharp.i8ckml0l/{0}/{1}/{2}.png"));
+
+            // define the mapview.
+            _mapView = new MapView(this, new MapViewSurface(this));
+            _mapView.Map = map;
+            _mapView.MapMaxZoomLevel = 18; // limit min/max zoom because MBTiles sample only contains a small portion of a map.
+            _mapView.MapMinZoomLevel = 0;
+            _mapView.MapTilt = 0;
+            _mapView.MapCenter = new GeoCoordinate(21.38, -157.782);
+            _mapView.MapZoom = 12;
+            _mapView.MapAllowTilt = false;
+
+            // set the map view as the default content view.
+            SetContentView(_mapView);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            // dispose of all resources.
+            // the mapview is completely destroyed in this sample, read about the Android Activity Lifecycle here:
+            // http://docs.xamarin.com/guides/android/application_fundamentals/activity_lifecycle/
+            _mapView.Dispose();
+            GC.Collect();
+        }
+    }
+}
